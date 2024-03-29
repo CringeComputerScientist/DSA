d7d27e6e78bfefa06b50acee86bcd74e3be1f0c5
Shashwat Sharma <119612629+CringeComputerScientist@users.noreply.github.com>
2024-03-29 20:33:36 +0530

    Update and rename maxheap.cpp to heap.cpp

--- Data Structures/heap.cpp
+++ Data Structures/heap.cpp
@@ -118,3 +118,120 @@ public:
                 return temp;
         }
 };
+
+class minheap
+{
+        int *arr;
+        int m;
+        int s;
+
+public:
+        minheap(int ma)
+        {
+                m = ma;
+                s = 0;
+                arr = new int[ma];
+        }
+
+        int parent(int n)
+        {
+                return (n - 1) / 2;
+        }
+
+        int leftchild(int n)
+        {
+                return (2 * n) + 1;
+        }
+
+        int rightchild(int n)
+        {
+                return (2 * n) + 2;
+        }
+
+        int maximum()
+        {
+                return arr[0];
+        }
+
+        int size()
+        {
+                return s;
+        }
+
+        void array()
+        {
+                for (int i = 0; i < s; i++)
+                {
+                        cout << arr[i] << " ";
+                }
+        }
+
+        void heapifyUp(int i)
+        {
+                while (i != 0 && arr[parent(i)] > arr[i])
+                {
+                        swap(arr[i], arr[parent(i)]);
+                        i = parent(i);
+                }
+        }
+
+        void insert(int x)
+        {
+                if (s == m)
+                {
+                        cout << "overflow error";
+                        return;
+                }
+                int i = s++;
+                arr[i] = x;
+                heapifyUp(i);
+        }
+
+        void delet(int x)
+        {
+                if (s == 0)
+                {
+                        cout << "underflow error";
+                        return;
+                }
+                arr[x] = arr[s - 1];
+                arr[--s] = 0;
+                heapifyDown(x);
+        }
+
+        void heapifyDown(int i)
+        {
+                int l = leftchild(i);
+                int r = rightchild(i);
+                int large = i;
+                if (l < s && arr[l] < arr[large])
+                {
+                        large = l;
+                }
+                if (r < s && arr[r] < arr[large])
+                {
+                        large = r;
+                }
+                if (large != i)
+                {
+                        swap(arr[i], arr[large]);
+                        heapifyDown(large);
+                }
+        }
+
+        int top()
+        {
+                if (s <= 0)
+                {
+                        return NULL;
+                }
+                if (s == 1)
+                {
+                        return arr[--s];
+                }
+                int temp = arr[0];
+                arr[0] = arr[--s];
+                heapifyDown(0);
+                return temp;
+        }
+};
